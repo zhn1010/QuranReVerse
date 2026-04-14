61e85d5aa871d81de4a330295ed60480a6bbb7bc
Saeed <saeed.torabzadeh@gmail.com>
2026-04-14 13:37:16 +0200

    adds force-dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import AntidoteWorkbench from '@/components/antidote-workbench';
 import { getQfUserSessionSummary } from '@/lib/qf-user';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const auth = await getQfUserSessionSummary();
 
